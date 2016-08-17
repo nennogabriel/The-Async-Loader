346747fc33802acda7a70c20b9d3f8d95496eb84
nennogabriel <nennogabriel@gmail.com>
2016-08-16 23:37:36 -0300

    encapsulate js

--- tal.js
+++ tal.js
@@ -1,43 +1,21 @@
-var tal = {
-    q: [], // list of queued libraries
-    r: [], // list of ready libraries
-    ready: function(list, callback, count){
-        if(!count){count=0}
-        if(typeof list == "string"){list = [list]}
-        for(var i in list){
-            if(charm.r.indexOf(list[i]) < 0){
-                setTimeout(function () { charm.ready(list, callback, count+1)}, 99);
-                return;
-            }
-        }
-        setTimeout(callback, 99);
-    },
-    load: function (url, reference, dependances) {
-        if(typeof reference == "string"){
-            if(charm.q.indexOf(reference) >= 0){return;}
-            charm.q.push(reference);
-        }
-        if(typeof dependances == "undefined"){
-            charm.get(url, reference);
-        } else {
-            charm.ready(dependances, function(){
-                charm.get(url, reference)
-            });
-        }
-    },
-    get: function(url, reference){
+var tal = (function(){
+    var _query = [],
+        _ready = [];
+
+
+    function _get(url, reference){
         var ext = url.split(".").pop(),
             js = ext == "js",
             el = document.createElement((js ? 'script' : 'link'));
         el.onload = el.onreadystatechange = function () {
             if (!el.readyState || el.readyState == 'loaded' || el.readyState == 'complete') {
-                if(typeof reference == "string"){charm.r.push(reference);}
+                if(typeof reference == "string"){_ready.push(reference);}
                 el.onload = el.onreadystatechange = null;
             }
         };
         if (js) {
             el.type = "text/javascript";
-            el.async = true;
+            // el.async = true;
             el.src = url;
         } else {
             el.rel = "stylesheet";
@@ -45,4 +23,35 @@ var tal = {
         }
         document.getElementsByTagName('head')[0].appendChild(el);
     }
-};
+
+    function load(url, reference, dependances) {
+        if(typeof reference == "string"){
+            if(_query.indexOf(reference) >= 0){return;}
+            _query.push(reference);
+        }
+        if(typeof dependances == "undefined"){
+            _get(url, reference);
+        } else {
+            ready(dependances, function(){
+                _get(url, reference)
+            });
+        }
+    }
+
+    function ready(values, callback, count){
+        var list = (typeof values == "string" ? [values] : values );
+        count = count || 0;
+        for(var i in list){
+            if(_ready.indexOf(list[i]) < 0){
+                setTimeout(function () { ready(list, callback, count+1)}, 99);
+                return;
+            }
+        }
+        setTimeout(callback, 99);
+    }
+    return {
+        ready: ready,
+        load: load
+    }
+
+})();
